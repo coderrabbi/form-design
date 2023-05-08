215055f7d313849ed2d6fd3410af0ff29bb7ab92
coderrabbie <golamrabbi.dv@gmail.com>
2023-05-08 21:55:21 +0600

    finished

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,14 +4,19 @@ import Register from "./Register";
 
 const Form = () => {
   return (
-    <div className="flex md:flex-row flex-col gap-4 ">
-      <div className="md:w-[40%]">
-        {" "}
-        <Login />
-      </div>
+    <div>
+      <h1 className="text-[32px] leading-[35px] py-3 text-[#333]">
+        Login / Register
+      </h1>
+      <div className="flex md:flex-row flex-col gap-4 ">
+        <div className="md:w-[40%]">
+          {" "}
+          <Login />
+        </div>
 
-      <div className="md:w-[55%]">
-        <Register className="flex-1.5" />
+        <div className="md:w-[55%]">
+          <Register className="flex-1.5" />
+        </div>
       </div>
     </div>
   );
